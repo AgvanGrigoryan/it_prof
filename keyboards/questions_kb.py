d01df99d25eddaebcc8f9d52b0c98d1b9f8fde14
AgvanGrigoryan <agvangrigoryan2003@gmail.com>
2022-09-10 19:27:43 +0400

    final demo 2

--- keyboards/questions_kb.py
+++ keyboards/questions_kb.py
@@ -22,11 +22,45 @@ q4_a2_btn = InlineKeyboardButton(q4_a2, callback_data='0')
 # Question 5
 q5_a1_btn = InlineKeyboardButton(q5_a1, callback_data='3')
 q5_a2_btn = InlineKeyboardButton(q5_a2, callback_data='0')
-
 # Question 6
 q6_a1_btn = InlineKeyboardButton(q6_a1, callback_data='3')
 q6_a2_btn = InlineKeyboardButton(q6_a2, callback_data='0')
-
+ # Question 7
+q7_a1_btn = InlineKeyboardButton(q7_a1, callback_data='3')
+q7_a2_btn = InlineKeyboardButton(q7_a2, callback_data='0')
+# Question 8
+q8_a1_btn = InlineKeyboardButton(q8_a1, callback_data='3')
+q8_a2_btn = InlineKeyboardButton(q8_a2, callback_data='0')
+# Question 9
+q9_a1_btn = InlineKeyboardButton(q9_a1, callback_data='3')
+q9_a2_btn = InlineKeyboardButton(q9_a2, callback_data='0')
+# Question 10
+q10_a1_btn = InlineKeyboardButton(q10_a1, callback_data='3')
+q10_a2_btn = InlineKeyboardButton(q10_a2, callback_data='0')
+# Question 11
+q11_a1_btn = InlineKeyboardButton(q11_a1, callback_data='3')
+q11_a2_btn = InlineKeyboardButton(q11_a2, callback_data='0')
+# Question 12
+q12_a1_btn = InlineKeyboardButton(q12_a1, callback_data='3')
+q12_a2_btn = InlineKeyboardButton(q12_a2, callback_data='0')
+# Question 13
+q13_a1_btn = InlineKeyboardButton(q13_a1, callback_data='3')
+q13_a2_btn = InlineKeyboardButton(q13_a2, callback_data='0')
+# Question 14
+q14_a1_btn = InlineKeyboardButton(q14_a1, callback_data='3')
+q14_a2_btn = InlineKeyboardButton(q14_a2, callback_data='0')
+# Question 15
+q15_a1_btn = InlineKeyboardButton(q15_a1, callback_data='3')
+q15_a2_btn = InlineKeyboardButton(q15_a2, callback_data='0')
+# Question 16
+q16_a1_btn = InlineKeyboardButton(q16_a1, callback_data='3')
+q16_a2_btn = InlineKeyboardButton(q16_a2, callback_data='0')
+# Question 17
+q17_a1_btn = InlineKeyboardButton(q17_a1, callback_data='3')
+q17_a2_btn = InlineKeyboardButton(q17_a2, callback_data='0')
+# Question 18
+q18_a1_btn = InlineKeyboardButton(q18_a1, callback_data='3')
+q18_a2_btn = InlineKeyboardButton(q18_a2, callback_data='0',)
 
 question_1_kb = InlineKeyboardMarkup().add(q1_a1_btn).add(q1_a2_btn).add(q1_a3_btn)
 
@@ -40,15 +74,48 @@ question_5_kb = InlineKeyboardMarkup().add(q5_a1_btn).add(q5_a2_btn)
 
 question_6_kb = InlineKeyboardMarkup().add(q6_a1_btn).add(q6_a2_btn)
 
+question_7_kb = InlineKeyboardMarkup().add(q7_a1_btn).add(q7_a2_btn)
 
-question_kb =\
-[
+question_8_kb = InlineKeyboardMarkup().add(q8_a1_btn).add(q8_a2_btn)
 
-    question_1_kb,
-    question_2_kb,
-    question_3_kb,
-    question_4_kb,
-    question_5_kb,
-    question_6_kb
+question_9_kb = InlineKeyboardMarkup().add(q9_a1_btn).add(q9_a2_btn)
 
-]
+question_10_kb = InlineKeyboardMarkup().add(q10_a1_btn).add(q10_a2_btn)
+
+question_11_kb = InlineKeyboardMarkup().add(q11_a1_btn).add(q11_a2_btn)
+
+question_12_kb = InlineKeyboardMarkup().add(q12_a1_btn).add(q12_a2_btn)
+
+question_13_kb = InlineKeyboardMarkup().add(q13_a1_btn).add(q13_a2_btn)
+
+question_14_kb = InlineKeyboardMarkup().add(q14_a1_btn).add(q14_a2_btn)
+
+question_15_kb = InlineKeyboardMarkup().add(q15_a1_btn).add(q15_a2_btn)
+
+question_16_kb = InlineKeyboardMarkup().add(q16_a1_btn).add(q16_a2_btn)
+
+question_17_kb = InlineKeyboardMarkup().add(q17_a1_btn).add(q17_a2_btn)
+
+question_18_kb = InlineKeyboardMarkup().add(q18_a1_btn).add(q18_a2_btn)
+
+question_kb = \
+    [
+        question_1_kb,
+        question_2_kb,
+        question_3_kb,
+        question_4_kb,
+        question_5_kb,
+        question_6_kb,
+        question_7_kb,
+        question_8_kb,
+        question_9_kb,
+        question_10_kb,
+        question_11_kb,
+        question_12_kb,
+        question_13_kb,
+        question_14_kb,
+        question_15_kb,
+        question_16_kb,
+        question_17_kb,
+        question_18_kb
+    ]
--- result.py
+++ result.py
@@ -4,24 +4,35 @@ from keyboards.client_kb import main_menu
 from questions import *
 from keyboards import questions_kb
 
-score = 0
-i = 0
+prof = dict()
+prof['a'] = 'Programmist'
+prof['b'] = 'Testirovshik'
+prof['c'] = 'Designer'
+prof['d'] = 'Info Bezaposnost'
+prof['e'] = 'Sis. admin'
+prof['f'] = 'marketing'
 
+prof_letter = dict()
+prof['a'] = 0
+prof['b'] = 0
+prof['c'] = 0
+prof['d'] = 0
+prof['e'] = 0
+prof['f'] = 0
+
+i = 0
 
 # not working
 async def cancel():
-    global i, score
+    global i
     i = 0
-    score = 0
 
 
 async def test(message: types.Message):
-    global i, score
-
+    global i
     if i == len(question):
-        await message.reply(str(score), reply_markup=main_menu)
-        i = 0
-        score = 0
+        res = await result()
+        await message.reply(res, reply_markup=main_menu)
     else:
         await message.reply(text=f"{i + 1}/{len(question)}\n" + question[i], reply_markup=questions_kb.question_kb[i],)
 
@@ -29,10 +40,13 @@ async def test(message: types.Message):
     i += 1
 
 
+async def result():
+    return prof[max(prof_letter, key=prof_letter.get)]
+
+
+
 async def count(cb):
-    global score
-    num = cb.data
-    score += int(num)
+    prof_letter[cb.data] +=1
     await test(cb.message)
     await cb.answer()
 
